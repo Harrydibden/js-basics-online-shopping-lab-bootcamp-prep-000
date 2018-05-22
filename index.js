4309557f6cd4ff641fed55f265c11a7fb4c921ff
Harry Dibden <harry-dibden@hotmail.com>
2018-05-22 22:30:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,5 +53,11 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber)
+  if(cardNumber === undefined){
+    return "Sorry, we don't have a credit card on file for you.";
+  } else {
+    var sentance = `Your total cost is $${total()}, which will be charged to the card ${cardNumber}`;
+    cart = [];
+    return sentance;
+  }
 }
