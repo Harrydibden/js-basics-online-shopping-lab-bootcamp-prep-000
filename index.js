8e0e66f0ce2e42ea27daa03ee4ef46bd725e60b1
Harry Dibden <harry-dibden@hotmail.com>
2018-05-22 22:21:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function total() {
 
 function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
-    if(cart[i].itemName == item){
+    if(cart[i].itemName === item){
       return cart.splice(i, 0);
     } else {
       return "That item is not in your cart."
